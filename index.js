006141380e0a9e7bf33e9fcd1f4bc4a941f69f6e
RenataCodes <psych.coaching@gmail.com>
2017-09-18 20:24:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,8 +29,8 @@ function prependKitten(name) {
 }
 
 function removeLastKitten() {
-var newArray = kittens.slice(0, kittens.length - 1)
-return newArray
+var newArray = kittens.pop()
+return kittens
 }
 
 function removeFirstKitten() {
